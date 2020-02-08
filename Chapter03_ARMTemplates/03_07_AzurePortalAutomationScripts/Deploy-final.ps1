f2878d8129e466389b9420b1fc84284bac248510
Rob Moore <me@robdmoore.id.au>
2020-02-08 17:29:37 +0800

    Added 03_07 files

---
+++ Chapter03_ARMTemplates/03_07_AzurePortalAutomationScripts/Deploy-final.ps1
@@ -0,0 +1,33 @@
+Param (
+    [string] [Parameter(Mandatory = $true)] $ResourceGroupName,
+    [string] [Parameter(Mandatory = $true)] $Location,
+    [string] [Parameter(Mandatory = $true)] $Environment
+)
+
+# Change current directory to the same folder as this script
+Set-Location $PSScriptRoot
+
+# Create a resource group with the name and location passed into the script
+az group create --name $ResourceGroupName --location $Location
+
+# Deploy the ARM template to the resource group
+$output = az group deployment create --name "chapter03_07_$Environment" --resource-group $ResourceGroupName --template-file ".\storage-website.json" --parameters environment=$Environment
+$output
+
+# Get the storageAccountName from the deployment output
+$StorageAccountName = (ConvertFrom-Json ($output -join "`r`n")).properties.outputs.storageAccountName.value
+
+# Create an index.html file in the current directory with a heading 1 element containing the environment name
+"<h1>$Environment</h1>" > index.html
+
+# Update the index.html file to the $web container in the storage account (where the static website files are contained)
+az storage blob upload -f ".\index.html" --container "`$web" --name "index.html" --account-name $StorageAccountName
+
+# Enable the static website feature within the storage service
+az storage blob service-properties update --account-name $StorageAccountName --static-website  --index-document index.html
+
+# Output the static website URL and the command to execute to delete the environment
+Write-Host "Static website URL:"
+Write-Host $(az storage account show -n $StorageAccountName -g $ResourceGroupName --query "primaryEndpoints.web" --output tsv)
+Write-Host "To remove this environment execute:"
+Write-Host "> az group delete --name $ResourceGroupName -y"
---
+++ Chapter03_ARMTemplates/03_07_AzurePortalAutomationScripts/README.md
@@ -0,0 +1,21 @@
+# Azure Resource Manager Templates: Azure Portal Automation Scripts
+
+We will be creating a static website using Storage that we can automatically deploy across environments by first creating it in the Azure Portal and then exporting it using the Azure Portal Automation Scripts feature to get the ARM template.
+
+Place the template in the `storage-website.json` file and then execute the deployment PowerShell script using the below commands to execute it for a test and production environment:
+
+```powershell
+.\Deploy.ps1 -ResourceGroupName acgarmsttcweb -Location southeastasia -Environment test
+.\Deploy.ps1 -ResourceGroupName acgarmsttcweb -Location southeastasia -Environment prod
+```
+
+You will need to add the following command to `Deploy.ps1` to enable the static website feature within the storage service:
+
+```powershell
+# Enable the static website feature within the storage service
+az storage blob service-properties update --account-name $StorageAccountName --static-website  --index-document index.html
+```
+
+If you don't want to run [PowerShell on your operating system](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell) then you can instead read the comments in the `Deploy.ps1` file to reverse engineer for your terminal of choice.
+
+If you want to see the final versions of the files see `Deploy-final.ps1` and `storage-website-final.json`.
