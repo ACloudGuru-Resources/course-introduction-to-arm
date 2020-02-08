87fc8ec9a38b18d7b5dc586161b1c0831b9574eb
Rob Moore <me@robdmoore.id.au>
2020-02-08 23:50:39 +0800

    Added 03_08 files

---
+++ Chapter03_ARMTemplates/03_08_AzureResourceExplorer/Deploy.ps1
@@ -0,0 +1,34 @@
+Param (
+    [string] [Parameter(Mandatory = $true)] $ResourceGroupName,
+    [string] [Parameter(Mandatory = $true)] $Location,
+    [string] [Parameter(Mandatory = $true)] $Environment
+)
+
+# Change current directory to the same folder as this script
+Set-Location $PSScriptRoot
+
+# Create a resource group with the name and location passed into the script
+az group create --name $ResourceGroupName --location $Location
+
+# Get current IP address
+$ip = Invoke-RestMethod http://ipinfo.io/json | Select-Object -exp ip
+
+# Deploy the ARM template to the resource group
+$output = az group deployment create --name "chapter03_07_$Environment" --resource-group $ResourceGroupName --template-file ".\storage-account.json" --parameters environment=$Environment
+$output
+
+# Get the storageAccountName from the deployment output
+$StorageAccountName = (ConvertFrom-Json ($output -join "`r`n")).properties.outputs.storageAccountName.value
+
+# Create a storage container called config
+az storage container create --account-name $StorageAccountName --name "config"
+
+# Create an config.json file in the current directory
+(Get-Content "config-template.json") -replace "{environment}",$Environment > config.json
+
+# Upload the config.json file to the config container in the storage account
+az storage blob upload -f ".\config.json" --container "config" --name "config.json" --account-name $StorageAccountName
+
+# Output the static website URL and the command to execute to delete the environment
+Write-Host "To remove this environment execute:"
+Write-Host "> az group delete --name $ResourceGroupName -y"
---
+++ Chapter03_ARMTemplates/03_08_AzureResourceExplorer/README.md
@@ -0,0 +1,16 @@
+# Azure Resource Manager Templates: Azure Resource Explorer
+
+We will be creating a storage account and deploying a config file to it. We will use Azure Resource Explorer to understand the syntax to allow IP addresses through the firewall.
+
+Execute the deployment PowerShell script using the below commands to execute it for a test and production environment:
+
+```powershell
+.\Deploy.ps1 -ResourceGroupName acgarmstorage -Location southeastasia -Environment test
+.\Deploy.ps1 -ResourceGroupName acgarmstorage -Location southeastasia -Environment prod
+```
+
+You will need to modify `storage-account.json` for the script to fully complete.
+
+If you don't want to run [PowerShell on your operating system](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell) then you can instead read the comments in the `Deploy.ps1` file to reverse engineer for your terminal of choice.
+
+If you want to see the final versions of the file see `storage-account-final.json`.
