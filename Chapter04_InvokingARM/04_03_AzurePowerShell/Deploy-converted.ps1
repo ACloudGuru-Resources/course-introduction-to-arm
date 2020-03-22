c8d79d15fa441105c8056426ed119d55732adf8f
Rob Moore <me@robdmoore.id.au>
2020-03-22 22:44:08 +0800

    Updated Deploy-converted.ps1

--- Chapter04_InvokingARM/04_03_AzurePowerShell/Deploy-converted.ps1
+++ Chapter04_InvokingARM/04_03_AzurePowerShell/Deploy-converted.ps1
@@ -33,13 +33,24 @@ $StorageContext = New-AzStorageContext -StorageAccountName $StorageAccountName
 Set-AzStorageBlobContent -File "./config.json" -Container "config" -Blob "config.json" -Context $StorageContext
 
 # Upload the index.html file to the $web container in the storage account
+# Previously: az storage blob upload -f "./index.html" --container "`$web" --name "index.html" --account-name $StorageAccountName --auth-mode key
 Set-AzStorageBlobContent -File "./index.html" -Container "`$web" -Blob "index.html" -Context $StorageContext
 
+
+
 # Enable the static website feature within the storage service
+# Previously: az storage blob service-properties update --account-name $StorageAccountName --static-website  --index-document index.html --auth-mode key
 Enable-AzStorageStaticWebsite -IndexDocument "index.html" -Context $StorageContext
+
+# Previously: az storage cors add --methods GET --origins "*" --services b --account-name $StorageAccountName
+# This command isn't actually needed anymore, since static-webapp.json has the CORS rules within it
+# However, if we did replace it, then it would look like: 
+#$corsRules = @(@{AllowedHeaders=@("*"); AllowedOrigins=@("*"); MaxAgeInSeconds=30; AllowedMethods=@("Get")})
+#Set-AzStorageCORSRule -CorsRules $corsRules -ServiceType Blob -Context $StorageContext
 
 # Output the static website URL and the command to execute to delete the environment
 Write-Host "Static website URL:"
+# Previously: Write-Host $(az storage account show -n $StorageAccountName -g $ResourceGroupName --query "primaryEndpoints.web" --output tsv)
 Write-Host (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName | Select-Object PrimaryEndpoints).PrimaryEndpoints.Web
 Write-Host "To remove this environment execute:"
 Write-Host "> az group delete --name $ResourceGroupName -y"
