20a8da1141e7c696d9fe73b8a03a4c265cc32fa6
Rob Moore <me@robdmoore.id.au>
2020-04-05 23:52:44 +0800

    05_04 done

---
+++ Chapter05_DebuggingARM/05_04_UsingDeploymentErrors/New-AzResourceGroupDeploymentWithErrorHandling.psm1
@@ -0,0 +1,17 @@
+function New-AzResourceGroupDeploymentWithErrorHandling($Name, $ResourceGroupName, $TemplateFile, $TemplateParameterObject) {
+    $deploymentName = ("$Name-" + (Get-Date -Format "yyyy-MM-dd-HH-mm-ss"))
+    $result = New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterObject $TemplateParameterObject -ErrorAction Continue -Verbose
+    Write-Host (ConvertTo-Json $result) -ForegroundColor Gray
+
+    if ((-not $result) -or ($result.ProvisioningState -ne "Succeeded")) {
+        Write-Error "Deployment failed; retrieving operation details..." -ErrorAction Continue
+        $operations = Get-AzResourceGroupDeploymentOperation -DeploymentName $deploymentName -ResourceGroupName $ResourceGroupName
+        $operations | ForEach-Object {
+            Write-Host "Operation $($_.id):" -ForegroundColor Magenta
+            Write-Host "Properties:" -ForegroundColor Cyan
+            Write-Host ($_.Properties | Select-Object -Property * -ExcludeProperty Request,Response | ConvertTo-Json -Depth 10).ToString() -ForegroundColor Gray
+        }
+        return $false
+    }
+    return $true
+}
--- Chapter05_DebuggingARM/05_04_UsingDeploymentErrors/README.md
+++ Chapter05_DebuggingARM/05_04_UsingDeploymentErrors/README.md
@@ -2,13 +2,25 @@
 
 We will interpret deployment errors from our deployments. There is guidance on deployment errors in the [Microsoft documentation](https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/common-deployment-errors).
 
-## View deployment errors in Portal
+## Prep
 
 Run the following commands:
 
 ```powershell
 az group create --name acgarmcourse0504 --location southeastasia
-az group deployment create --name "chapter05_04" --resource-group acgarmcourse0505 --template-file "./azuredeploy-error.json" --parameters environment=dev
+```
+
+## Azure CLI
+
+```powershell
+az group deployment create --name "chapter05_04_cli" --resource-group acgarmcourse0504 --template-file "./azuredeploy-error.json" --parameters environment=dev
+```
+
+## Azure PowerShell
+
+```powershell
+    Import-Module "./New-AzResourceGroupDeploymentWithErrorHandling.psm1" -Force
+    New-AzResourceGroupDeploymentWithErrorHandling -Name "chapter05_04_pwsh" -ResourceGroupName acgarmcourse0504 -TemplateFile "./azuredeploy-error.json" -TemplateParameterObject @{"environment"="test"}
 ```
 
 ## Clean up
